9dc02bd50eda752f17b0af347fa585fef357c5fb
gz-lenk <gz595740361@outlook.com>
2025-10-05 18:15:57 +0800

    complete Write C

--- gemmKernel.hpp
+++ gemmKernel.hpp
@@ -34,12 +34,16 @@ class GemmKernel {
         MemStream& p_Bs
     ) {
         for(int l_aRowBlock = 0; l_aRowBlock < l_aRowBlocks; ++l_aRowBlock) {
+            #pragma HLS LOOP_TRIPCOUNT min=BLAS_gemmMBlocks max=BLAS_gemmMBlocks avg=BLAS_gemmMBlocks
             for(int l_bColBlock = 0; l_bColBlock < l_bColBlocks; ++l_bColBlock) {
+                #pragma HLS LOOP_TRIPCOUNT min=BLAS_gemmNBlocks max=BLAS_gemmNBlocks avg=BLAS_gemmNBlocks
                 for(int l_aColBlock = 0; l_aColBlock < l_aColBlocks; ++l_aColBlock) {
+                    #pragma HLS LOOP_TRIPCOUNT min=BLAS_gemmKBlocks max=BLAS_gemmKBlocks avg=BLAS_gemmKBlocks
                     // l_bufferB
                     for (int i = 0; i < t_bKD; ++i){
                         #pragma HLS PIPELINE II=t_bColMemWords
                         for (int j = 0; j < t_bColMemWords; ++j){
+                            #pragma HLS LOOP_TRIPCOUNT min=BLAS_memWidth max=BLAS_memWidth avg=BLAS_memWidth
                             unsigned int l_bAddrIdx = (l_bColBlock * t_bColMemWords + j) + (i + l_aColBlock * t_bKD) * l_bWordLd;
                             MemIntType l_bVal = l_bAddr[l_bAddrIdx];
                             p_Bs.write(l_bVal);
@@ -49,6 +53,7 @@ class GemmKernel {
                     for (int i = 0; i < t_aMH; i++){  // Number of matrix elements in one col of matrix A buffer
                         #pragma HLS PIPELINE II = t_aColMemWords
                         for (int j = 0; j < t_aColMemWords; j++) {
+                            #pragma HLS LOOP_TRIPCOUNT min=BLAS_memWidth max=BLAS_memWidth avg=BLAS_memWidth
                             unsigned int l_aSrcOffset =
                                 l_aWordLd * t_aMH * l_aRowBlock + l_aColBlock * t_aColMemWords + i * l_aWordLd + j;
                             MemIntType l_word = l_aAddr[l_aSrcOffset];
@@ -94,7 +99,39 @@ class GemmKernel {
 
     }
 
-    void GemmWriteMemStream(){}
+
+    /**
+     * @brief Write the result matrix C from stream to memory
+     * @param l_cAddr the base address of matrix C in external memory
+     * @param p_Cs the stream of matrix C blocks
+     * @param l_aRowBlocks number of row blocks of matrix A
+     * @param l_bColBlocks number of column blocks of matrix B
+     * @param l_cWordLd leading dimension of matrix C in memory, in unit of memory words
+     */
+    void GemmWriteMemStream(
+        MemIntType* l_cAddr,
+        MemStream& p_Cs,
+        unsigned int l_aRowBlocks,
+        unsigned int l_bColBlocks,
+        unsigned int l_cWordLd
+    ){
+        unsigned int l_rowOffset = 0;
+        unsigned int l_colOffset = 0;
+
+        for (int rowBlock = 0; rowBlock < l_aRowBlocks; ++rowBlock) {
+            for (int colBlock = 0; colBlock < l_bColBlocks; ++colBlock) {
+                for (int i = 0; i < t_aMH; i++) {
+                    #pragma HLS PIPELINE II = t_aColMemWords
+                    for (int j = 0; j < t_bColMemWords; j++) {
+                        unsigned int l_cDstOffset =
+                            l_cWordLd * t_aMH * rowBlock + colBlock * t_bColMemWords + i * l_cWordLd + j;
+                        MemIntType l_word = p_Cs.read();
+                        l_cAddr[l_cDstOffset] = l_word;
+                    }
+                }
+            }
+        }
+    }
 
     void GemmBlocks(
         MemIntType* p_aAddr,
